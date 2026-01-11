c03ef82a781f8132d9a2511f0189ed72a6cc02a9
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:16:46 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "virtual_desktop_host_pools" {
+  description = "All virtual_desktop_host_pool resources"
+  value       = azurerm_virtual_desktop_host_pool.virtual_desktop_host_pools
+}
+
---
+++ variables.tf
@@ -0,0 +1,67 @@
+variable "virtual_desktop_host_pools" {
+  description = <<EOT
+Map of virtual_desktop_host_pools, attributes below
+Required:
+    - load_balancer_type
+    - location
+    - name
+    - resource_group_name
+    - type
+Optional:
+    - custom_rdp_properties
+    - description
+    - friendly_name
+    - maximum_sessions_allowed
+    - personal_desktop_assignment_type
+    - preferred_app_group_type
+    - public_network_access
+    - start_vm_on_connect
+    - tags
+    - validate_environment
+    - vm_template
+    - scheduled_agent_updates (block):
+        - enabled (optional)
+        - schedule (optional, block):
+            - day_of_week (required)
+            - hour_of_day (required)
+        - timezone (optional)
+        - use_session_host_timezone (optional)
+EOT
+
+  type = map(object({
+    load_balancer_type               = string
+    location                         = string
+    name                             = string
+    resource_group_name              = string
+    type                             = string
+    custom_rdp_properties            = optional(string)
+    description                      = optional(string)
+    friendly_name                    = optional(string)
+    maximum_sessions_allowed         = optional(number, 999999)
+    personal_desktop_assignment_type = optional(string)
+    preferred_app_group_type         = optional(string, "Desktop")
+    public_network_access            = optional(string, "Enabled")
+    start_vm_on_connect              = optional(bool, false)
+    tags                             = optional(map(string))
+    validate_environment             = optional(bool, false)
+    vm_template                      = optional(string)
+    scheduled_agent_updates = optional(object({
+      enabled = optional(bool, false)
+      schedule = optional(list(object({
+        day_of_week = string
+        hour_of_day = number
+      })))
+      timezone                  = optional(string, "UTC")
+      use_session_host_timezone = optional(bool, false)
+    }))
+  }))
+  validation {
+    condition = alltrue([
+      for k, v in var.virtual_desktop_host_pools : (
+        v.scheduled_agent_updates.schedule == null || (length(v.scheduled_agent_updates.schedule) <= 2)
+      )
+    ])
+    error_message = "Each schedule list must contain at most 2 items"
+  }
+}
+
